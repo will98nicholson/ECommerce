5cb987a80b33fef806d7f9dfa794cb1736b43abe
William Nicholson <will98nicholson@gmail.com>
2021-04-22 23:26:04 -0400

    index js model done

--- models/index.js
+++ models/index.js
@@ -5,13 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, { foreignKey: 'category_id' });
 // Categories have many Products
-
+Category.belongsTo(Product, { foreignKey: 'category_id' });
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: ProductTag });
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(ProductTag, { through: ProductTag });
 module.exports = {
     Product,
     Category,
