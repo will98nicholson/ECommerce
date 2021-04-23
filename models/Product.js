087c3d5e1511df66c02e69c7e34b64c77a2bbd13
William Nicholson <will98nicholson@gmail.com>
2021-04-23 10:30:56 -0400

    product model

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,39 @@ class Product extends Model { }
 Product.init(
     {
         // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 10,
+            validate: {
+                isNumeric: true
+            }
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Category',
+                key: 'id',
+                unique: false
+            }
+        }
     },
     {
         sequelize,
